fa92d7ea1a4b789a06e48c5b4cfe2fb71699d9ce
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2021-07-20 21:08:41 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDjMyM9YzVcty2Rcf7rtkrJ3b0nAaYFFmY",
+    authDomain: "lets-chat-web-app-9bfb7.firebaseapp.com",
+    projectId: "lets-chat-web-app-9bfb7",
+    storageBucket: "lets-chat-web-app-9bfb7.appspot.com",
+    messagingSenderId: "50012822852",
+    appId: "1:50012822852:web:92642f5950867e3792cb11",
+    measurementId: "G-B8XZYQ3ZPQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
